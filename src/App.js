b492f48683a3902655827bb67fc5799d0df5b5ab
fabionasc1mento <fabiotcn333@gmail.com>
2024-06-22 19:43:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import "./style.css";
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    if(input === ''){
+      alert('Preencha o campo vazio!');
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+    }catch{
+      alert('Ops! Erro ao buscar.');
+      setInput('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="container">
+        <h1 className="title">Buscador CEP</h1>
+
+        <div className="container-input">
+          <input type="text" 
+          placeholder="digite o CEP..."
+          value={input}
+          onChange={(event)=>setInput(event.target.value)}
+          />
+
+          <button className="btn" onClick={handleSearch}>
+            <FiSearch size={25} color='#fff'/>
+          </button> 
+
+        </div>
+
+        {Object.keys(cep).length > 0 && (
+          <main className='main'>
+            <h2>CEP: {cep.cep}</h2>
+            <span>Rua: {cep.logradouro}</span>
+            <span>Num: {cep.complemento}</span>
+            <span>Bairro: {cep.bairro}</span>
+            <span>Cidade: {cep.localidade} - {cep.uf}</span>
+            <span>DDD: {cep.ddd}</span>
+          </main >
+        )}
+
+      </div>
+    </body>
   );
 }
 
